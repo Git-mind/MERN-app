f7f133c3c1b0a605827777a0cb15b3a7cb8ebed7
Xianweiii <68322946+Git-mind@users.noreply.github.com>
2022-10-04 13:52:55 +0800

    Add dotenv to store port and connection variable

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from './routes/post.js';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -16,12 +18,12 @@ app.use('/posts', postRoutes);
 
 //https://www.mongodb.com/cloud/atlas
 
-const CONNECTION_URL =
-    "mongodb+srv://user-123:user123@cluster0.ver59ms.mongodb.net/?retryWrites=true&w=majority";
+// const CONNECTION_URL =
+//     "mongodb+srv://user-123:user123@cluster0.ver59ms.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-    .connect(CONNECTION_URL, {
+    .connect(process.env.CONNECTION_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
